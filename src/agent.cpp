bcfdd40b16b107f4e967105b99da5c017144281c
xiuwen <xiuwen.he@dorabot.com>
2019-06-24 23:43:03 +0800

    add simple logic of whole process, need to update detail later

--- src/agent.cpp
+++ src/agent.cpp
@@ -2,12 +2,13 @@
 #include "std_msgs/String.h"
 #include <ctime>
 
-void agent_feedback_callback(const )
+std::atomic<bool> new_task;
+std::time_t receive_task_timepoint;
+
+void agent_task_callback(const )
 {
 
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -16,13 +17,29 @@ int main(int argc, char **argv)
   std::stringstream time_stream;
   time_stream << seconds;
   std::string agent_node_name = "simple_agent" + time_stream.str ();
+  std::string agent_task_topic = agent_node_name + "_task";
+
+  new_task.store(false);
 
   ros::init(argc, argv, agent_node_name);
 
   ros::NodeHandle agent_handler;
 
-  ros::Publisher agent_pub = agent_handler.advertise("/agent_feedback", 10);
+  ros::Publisher agent_feedback_pub = agent_handler.advertise("/agent_feedback", 10);
+  ros::Publisher agent_register_pub = agent_handler.advertise("/agent_register", 10);
 
-  ros::
+  ros::Subscriber agent_task_sub = agent_handler.subscribe(agent_task_topic,
+                                                          10,
+                                                          agent_task_callback);
 
+  agent_register_pub.publish();
+
+  while(true)
+  {
+    if (new_task.load())
+    {
+
+    }
+    agent_feedbakc_pub.publish();
+  }
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,20 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+std::list<int> task_list; // task list
+
+// key : agent id; value : free(true) or busy(false)
+std::map<int, bool> agent_status_map;
+
+// key : agent id; value : agent node name
+std::map<int, std::string> agent_node_name_map;
+
 void agent_feedback_callback(const )
+{
+
+}
+
+void agent_register_callback(const )
 {
 
 }
@@ -12,8 +25,35 @@ int main(int argc, char **argv)
 
   ros::NodeHandle server_handler;
 
-  ros::Subscriber sub = server_handler.subscribe("/agent_feedback",
-                                                 10,
-                                                 agent_feedback_callback);
+  ros::Subscriber feedback_sub = server_handler.subscribe("/agent_feedback",
+                                                          10,
+                                                          agent_feedback_callback);
+
+  ros::Subscriber register_sub = server_handler.subscribe("/agent_register",
+                                                          10,
+                                                          agent_register_callback);
+
+  while(true)
+  {
+    for (auto agent_iter = agent_status_map.cbegin();
+         agent_iter != agent_status_map.cend();
+         agent_iter++)
+    {
+      if (agent_iter->second == "Free")
+      {
+        if (!task_list.empty())
+        {
+          std::string agent_task_topic
+            = agent_node_name_map.at(agent_iter->first) + "_task";
+
+          ros::Publisher temp_task_pub = server_handler.advertise(agent_task_topic, 10);
+
+          temp_task_pub.publish();
+        }
+      }
+
+    }
+
+  }
 
 }
